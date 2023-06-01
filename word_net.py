c49954d04f73281cfa3ea0c61219d8ed6e96baa8
Tomi <tomicaceres07@gmail.com>
2023-06-01 12:07:07 -0300

    Neural network with top 100 words with the highest degree

--- word_net.py
+++ word_net.py
@@ -1,69 +1,50 @@
 import pandas as pd
+import networkx as nx
 from pyvis.network import Network
-import random
 
-# Create a sample dataframe with 50,000 edges
-n = 50000
-data = {
-    'Source': [f'Word_{i}' for i in range(n)],
-    'Target': [f'Word_{i+1}' for i in range(n)],
-    'Weight': [random.randint(1, 10) for _ in range(n)]
-}
+# Load the data from the CSV file
+df = pd.read_csv('./words_nx.csv')
 
-df = pd.DataFrame(data)
+# Create a networkx graph
+G = nx.from_pandas_edgelist(df, 'Origen', 'Destino', ['frequency'])
+
+# Get the top 100 words with the highest degree
+top_words = sorted(G.degree, key=lambda x: x[1], reverse=True)[:100]
+
+# Create a subgraph with the top words
+subgraph = G.subgraph([word for word, _ in top_words])
+
+# Calculate node sizes based on degree
+node_sizes = dict(subgraph.degree())
+max_degree = max(node_sizes.values())
+node_sizes = {node: size / max_degree * 100 for node, size in node_sizes.items()}
+
+# Calculate node degrees for the subgraph
+node_degrees = dict(subgraph.degree())
+
+# Calculate edge widths based on node degrees
+edge_widths = {(source, target): node_degrees[source] + node_degrees[target] for source, target in subgraph.edges()}
+
+# Normalize edge widths to a reasonable range for visualization
+max_width = max(edge_widths.values())
+edge_widths = {edge: width / max_width * 10 for edge, width in edge_widths.items()}
 
 # Create a network visualization
 network = Network(height="750px", width="100%", notebook=False)
 
-# Get the top 50 words with the highest degree
-top_words = df['Source'].value_counts().nlargest(50).index.tolist()
+# Add nodes with custom sizes and labels
+for node, size in node_sizes.items():
+    network.add_node(node, size=size, label=node, title=node, font={'size': size})
 
-# Add nodes
-network.add_nodes(top_words)
-
-# Add edges
-filtered_df = df[df['Source'].isin(top_words) & df['Target'].isin(top_words)]
-edges = zip(filtered_df['Source'], filtered_df['Target'], filtered_df['Weight'])
-for source, target, weight in edges:
-    network.add_edge(source, target, value=weight)
+# Add edges with custom widths
+for edge, width in edge_widths.items():
+    source, target = edge
+    network.add_edge(source, target, value=width, width=width)
 
 # Set the physics layout of the network
-network.barnes_hut()
+network.barnes_hut(gravity=-17650, central_gravity=0.3)
+
+network.show_buttons(filter_=['physics'])
 
 # Save the network as an HTML file
-network.set_options(
-    """
-    const options = {
-  "nodes": {
-    "borderWidth": null,
-    "borderWidthSelected": null,
-    "opacity": null,
-    "font": {
-      "size": 81
-    },
-    "size": 117
-  },
-  "edges": {
-    "color": {
-      "inherit": true
-    },
-    "selfReferenceSize": null,
-    "selfReference": {
-      "angle": 0.7853981633974483
-    },
-    "smooth": {
-      "forceDirection": "none"
-    }
-  },
-  "physics": {
-    "barnesHut": {
-      "gravitationalConstant": -80000,
-      "springLength": 250,
-      "springConstant": 0.001
-    },
-    "minVelocity": 0.75
-  }
-}
-    """
-)
 network.show("network.html")
